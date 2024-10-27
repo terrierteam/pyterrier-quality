2296c8335607003a10c683a5a19ca44485575282
Sean MacAvaney <sean.macavaney@gmail.com>
2024-10-27 20:41:45 +0000

    Update filter.py

--- pyterrier_quality/filter.py
+++ pyterrier_quality/filter.py
@@ -5,6 +5,6 @@ class Filter(pt.Transformer):
     def __init__(self, min_quality_score: float):
         self.min_quality_score = min_quality_score
 
-    def forward(self, inp):
+    def transform(self, inp):
         assert 'quality' in inp
         return inp[inp['quality'] >= min_quality_score]
